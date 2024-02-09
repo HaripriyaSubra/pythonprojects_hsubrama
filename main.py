bd2646e6ff0460c02e0ec7b5da6a401360c175b7
hsubramagitacc <134923817+hsubramagitacc@users.noreply.github.com>
2024-02-09 13:16:09 +0100

    first program

---
+++ main.py
@@ -0,0 +1,22 @@
+
+import datetime
+
+name = input('Enter your name:')
+
+hour = datetime.datetime.now().hour
+
+
+def find_greeting(current_hour):
+    match current_hour:
+        case 21 | 22 | 23 | 1 | 2 | 3 | 4 | 5:
+            greeting = "Good Night";
+        case 6 | 7 | 8 | 9 | 10 | 11 | 12:
+            greeting = "Good Morning";
+        case 13 | 14 | 15:
+            greeting = "Good Afternoon";
+        case 16 | 17 | 18 | 19 | 20:
+            greeting = "Good Evening";
+    return greeting;
+
+
+print(f'Hello {name}. ' + find_greeting(hour))

c897aecd28dbbda5b902adce8ef6684d3b21cc41
hsubramagitacc <134923817+hsubramagitacc@users.noreply.github.com>
2024-02-09 14:33:45 +0100

    sorting_arrays

--- array_file_test.py
+++ array_file_test.py
@@ -10,6 +10,16 @@ class ArrayFileTest(unittest.TestCase):
         print(len(non_dup_array))
         self.assertEqual(len(non_dup_array), 5)
 
+    def test_sort_ascending(self):
+        numbers = [56, 45, 23, 67, 34]
+        sorted_array = arraya_file.sort_ascending(numbers)
+        self.assertEqual(sorted_array, [23, 34, 45, 56, 67])
+
+    def test_sort_descending(self):
+        numbers = [56, 45, 23, 67, 34]
+        sorted_array = arraya_file.sort_descending(numbers)
+        self.assertEqual(sorted_array, [67, 56, 45, 34, 23])
+
 
 if __name__ == '__main__':
     unittest.main()
--- arraya_file.py
+++ arraya_file.py
@@ -1,11 +1,19 @@
-numbers = [1, 2, 3, 3, 5, 6, 2, 1]
-
-
 def remove_duplicates(numbers_array_with_duplicates):
     non_dup_array = []
     for num in numbers_array_with_duplicates:
         if num not in non_dup_array:
             non_dup_array.append(num)
     return non_dup_array
+
+
+def sort_ascending(numbers):
+    sorted_array = sorted(numbers)
+    return sorted_array
+
+
+def sort_descending(numbers):
+    sorted_array = sorted(numbers, reverse=True)
+    return sorted_array
+
 
 
